8ac71b83f466c1846f3d8ef2c61166d5346b4eb2
bean-123 <amy.platt@hotmail.com>
2026-03-30 21:04:06 +0300

    Add fix to route

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,18 +1,18 @@
 // API call to supabase using REST API
-const supabaseURL = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-
-// Direct API call to supabase using fetch
-const response = await fetch(`${supabaseURL}/rest/v1/products`, {
-  headers: {
-    Authorization: `Bearer ${supabaseAnonKey}`,
-    apiKey: supabaseAnonKey,
-    "Content-Type": "application/json",
-  },
-});
-
-const data = await response.json();
 
 export async function GET() {
+  const supabaseURL = process.env.SUPABASE_URL!;
+  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+
+  const response = await fetch(`${supabaseURL}/rest/v1/products`, {
+    headers: {
+      Authorization: `Bearer ${supabaseAnonKey}`,
+      apikey: supabaseAnonKey, // lowercase preferred
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
   return Response.json(data);
 }
